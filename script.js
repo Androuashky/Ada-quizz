bb21a1ff4a22caaaf8681f1ffb95a4192fcf8d6a
Elisa Chour <elisa.chour@gmail.com>
2026-06-11 14:19:35 +0200

    realisation des 3 niveaux js

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const lancementButton = document.getElementById("lancement");
-const quizSection = document.getElementById("quiz");
 const accueilSection = document.getElementById("accueil");
 const pageniveaux = document.getElementById("niveaux")
-const questionniveau1 = document.getElementById ("questionfacile")
+const questionniveau1 = document.getElementById ("facile")
+const questionniveau2 = document.getElementById ("moyen")
+const questionniveau3 = document.getElementById ("difficile")
 
-lancementButton.addEventListener("click", startQuiz);
+lancementButton.addEventListener("click", afficherNiveaux);
 
-function startQuiz (){
+function afficherNiveaux (){
     pageniveaux.hidden=false;
     accueilSection.hidden=true;
 }
 
-function startQuizniveaux (){
-    pageniveaux.hidden=true;
-    accueilSection.hidden=true;
 
+questionniveau1.addEventListener("click", afficherFacile);
+
+function afficherFacile () {
+    pageniveaux.hidden=true;
+    questionniveau1.hidden=false;
 }
 
-lancementButton.addEventListener("click", startQuizniveaux);
+questionniveau2.addEventListener("click", afficherMoyen);
+
+function afficherMoyen () {
+    pageniveaux.hidden=true;
+    questionniveau2.hidden=false;
+}
+
+questionniveau3.addEventListener("click", afficherDifficile);
+
+function afficherDifficile () {
+    pageniveaux.hidden=true;
+    questionniveau3.hidden=false;
+}
