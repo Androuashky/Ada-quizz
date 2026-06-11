95553310ef79874c6113780fc2d4b2fe3d49f800
Elisa Chour <elisa.chour@gmail.com>
2026-06-11 10:30:58 +0200

    Afficher les niveaux

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const lancementButton = document.getElementById("lancement");
 const quizSection = document.getElementById("quiz");
 const accueilSection = document.getElementById("accueil");
+const pageniveaux = document.getElementById("niveaux")
+const questionniveau1 = document.getElementById ("questionfacile")
 
 lancementButton.addEventListener("click", startQuiz);
 
 function startQuiz (){
-    quizSection.hidden=false;
+    pageniveaux.hidden=false;
     accueilSection.hidden=true;
 }
+
+function startQuizniveaux (){
+    pageniveaux.hidden=true;
+    accueilSection.hidden=true;
+
+}
+
+lancementButton.addEventListener("click", startQuizniveaux);
